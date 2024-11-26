fdac77336957e89290acb73cfbc3e6c8fb2f8a56
Tanya-Srivastava <tanyasrivastava2808@gmail.com>
2024-11-26 23:56:28 +0530

    footer started

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-import { Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
 import Cart from './pages/Cart/Cart.jsx'
 import Placeorder from './pages/PlaceOrder/Placeorder.jsx'
+import Footer from './components/Footer/Footer.jsx'
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/cart' element={<Cart/>} />
-        <Route path='/order' element={<Placeorder/>} />
-      </Routes>
-    </div>
+    <>
+      <div className='app'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<Placeorder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+
   );
 }
 
