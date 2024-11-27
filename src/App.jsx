cbe3a568b409fc52bb0d8702469cd7613c227d64
Tanya-Srivastava <tanyasrivastava2808@gmail.com>
2024-11-27 19:26:42 +0530

    login page started

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,16 @@ import Home from './pages/Home/Home.jsx'
 import Cart from './pages/Cart/Cart.jsx'
 import Placeorder from './pages/PlaceOrder/Placeorder.jsx'
 import Footer from './components/Footer/Footer.jsx'
+import LoginPopup from './components/LoginPopup/LoginPopup.jsx'
 
 const App = () => {
+
+  const [showLogin,setShowLogin] = useState(false)
   return (
     <>
+    {showLogin?<LoginPopup/>:<></>}
       <div className='app'>
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
