3256f25e16bcca7a6c2918c48c7ca448aeb43f5d
Tanya-Srivastava <tanyasrivastava2808@gmail.com>
2024-11-28 21:48:36 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home.jsx'
-import Cart from './pages/Cart/Cart.jsx'
-import Placeorder from './pages/PlaceOrder/Placeorder.jsx'
-import Footer from './components/Footer/Footer.jsx'
-import LoginPopup from './components/LoginPopup/LoginPopup.jsx'
+import React, { useState } from 'react'; // Added useState import
+import Navbar from './components/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home.jsx';
+import Cart from './pages/Cart/Cart.jsx';
+import Placeorder from './pages/PlaceOrder/Placeorder.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import LoginPopup from './components/LoginPopup/LoginPopup.jsx';
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false); 
 
-  const [showLogin,setShowLogin] = useState(false)
   return (
     <>
-    {showLogin?<LoginPopup/>:<></>}
-      <div className='app'>
-        <Navbar setShowLogin={setShowLogin} />
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>} 
+      <div className="app">
+        <Navbar setShowLogin={setShowLogin} /> {/* Passing setShowLogin to Navbar */}
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/order' element={<Placeorder />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<Placeorder />} />
         </Routes>
       </div>
       <Footer />
     </>
-
   );
-}
+};
 
 export default App;
--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginPopup.css'
+import { assets } from '../../assets/assets'
 
-const LoginPopup = () => {
+const LoginPopup = ({setShowLogin}) => {
+
+  const [currentState,setCurrentState] = useState("Sign Up")
   return (
     <div className='login-popup'> 
-      
+     <form className="login-popup-container">
+      <div className="login-popup-title">
+        <h2>{currentState}</h2>
+        <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
+      </div>
+      <div className="login-popup-inputs">
+         <input type="text" placeholder='Your name' required />
+        <input type="Email" placeholder='Your Email' required/>
+        <input type="Password" placeholder='Your Password' required/>
+      </div>
+      <button>"Create account"</button>
+     </form>
     </div>
   )
 }
